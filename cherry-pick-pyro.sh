edac897b56d1a849ff792dc191167b00f3edf00c
Alisson L <alissonvitortc@gmail.com>
2023-11-02 21:02:30 -0300

    feat: improve cherry-picker to avoid merge conflicts
    * feat: improve cherry-picker to avoid merge conflicts
    * fix: fix changes getter

--- cherry-pick-pyro.sh
+++ cherry-pick-pyro.sh
@@ -2,113 +2,197 @@
 set -e
 
 # Config variables
-
-# The Pyrogram repository to pull from
 ORIGIN_REPO="pyrogram/pyrogram"
-# The branch to pull from
 ORIGIN_BRANCH="master"
 
+RUFF_TOML=$(cat <<EOF
+[tool.ruff]
+line-length = 99
+target-version = "py38"
+select = [
+    "I",    # isort
+    "E",    # pycodestyle
+    "W",    # pycodestyle
+    "UP",   # pyupgrade
+    "F",    # pyflakes
+    "SIM",  # flake8-simplify
+    "RET",  # flake8-return
+    "C4",   # flake8-comprehensions
+    "PTH",  # flake8-use-pathlib
+    "PERF", # perflint
+    "N",    # pep8-naming
+    "RUF",  # ruff
+]
+ignore = ["RUF001", "RUF002", "RUF003", "E203", "F401", "F403"]
+preview = true
 
-FORMATTED_ORIGIN_BRANCH="formatted-$ORIGIN_BRANCH"
+[tool.ruff.isort]
+known-first-party = ["hydrogram"]
+EOF
+)
 
-if { [ "$1" != "pr" ] && [ "$1" != "branch" ] && [ "$1" != "commit" ]; } || [ -z "$2" ]; then
-    echo "Usage: $0 pr <PR number>"
-    echo "       $0 branch <branch name>"
-    echo "       $0 commit <commit hash>"
-    exit 1
-fi
+hydrogramify() {
+    # Create a pyproject.toml file
+    echo "$RUFF_TOML" > pyproject.toml
 
-CP_TYPE=$1
-CP_DATA=$2
-CWD=$(pwd)
+    # Rename everything from "pyrogram" to "hydrogram"
+    echo "Renaming everything from \"pyrogram\" to \"hydrogram\"…"
+    mv pyrogram hydrogram
+    grep -rIl "pyrogram" --exclude-dir=.git . | xargs sed -i 's|pyrogram|hydrogram|g;s|Pyrogram|Hydrogram|g'
 
-TARGET_BRANCH="$CP_TYPE-$CP_DATA"
+    # Format the code
+    echo "Formatting code…"
+    ruff check --quiet --fix --unsafe-fixes . > /dev/null 2>&1 || true
+    ruff format --quiet .
 
+    # Commit the changes
+    git add -- . ':!pyproject.toml'
+    git commit --quiet -m "Format code"
+}
 
-# Clone the Pyrogram repository into a temporary directory
-echo "Cloning Pyrogram repository…"
-TEMP_DIR=$(mktemp -d)
-git clone --quiet https://github.com/$ORIGIN_REPO.git "$TEMP_DIR"
+checkout_code() {
+    if [ "$CP_TYPE" == "pr" ]; then
+        echo "Checking out PR code…"
 
-# Switch to the $FORMATTED_ORIGIN_BRANCH branch
-cd "$TEMP_DIR"
-git switch --quiet -c "$FORMATTED_ORIGIN_BRANCH"
+        git fetch --quiet origin "pull/$CP_DATA/head:$TARGET_BRANCH"
+        git switch --quiet "$TARGET_BRANCH"
+    elif [ "$CP_TYPE" == "branch" ]; then
+        echo "Checking out branch code…"
 
-# Format the code
-echo "Formatting $ORIGIN_BRANCH code…"
-ruff format --quiet .
+        git switch --quiet -c "$TARGET_BRANCH" "$ORIGIN_BRANCH"
+        git cherry-pick --quiet "$CP_DATA" > /dev/null 2>&1
+    elif [ "$CP_TYPE" == "commit" ]; then
+        echo "Checking out commit code…"
 
-# Commit the changes
-git commit --quiet -am "Format code"
+        git switch --quiet -c "$TARGET_BRANCH" "$ORIGIN_BRANCH"
+        git cherry-pick --quiet "$CP_DATA" > /dev/null 2>&1
+    fi
+}
 
-# Fetch commits/branches to make them available for cherry-picking
-if [ "$CP_TYPE" == "branch" ] || [ "$CP_TYPE" == "commit" ]; then
-    git fetch --quiet "$CWD" "$CP_DATA" || git fetch --quiet https://github.com/$ORIGIN_REPO.git "$CP_DATA" || true
-fi # PR
+create_patch() {
+    # Create a patch between the two branches
+    echo "Creating patch and saving to /tmp/$TARGET_BRANCH.patch…"
+    git diff "$FORMATTED_ORIGIN_BRANCH" "$TARGET_BRANCH" > "/tmp/$TARGET_BRANCH.patch"
+}
 
-# Checkout the code
-if [ "$CP_TYPE" == "pr" ]; then
-    echo "Checking out PR code…"
+apply_patch_to_formatted() {
+    # Switch back to the $FORMATTED_ORIGIN_BRANCH branch
+    git switch --quiet "$FORMATTED_ORIGIN_BRANCH"
 
-    git fetch --quiet origin "pull/$CP_DATA/head:$TARGET_BRANCH"
-    git switch --quiet "$TARGET_BRANCH"
-elif [ "$CP_TYPE" == "branch" ]; then
-    echo "Checking out branch code…"
+    # Apply the patch to the formatted branch
+    echo "Applying patch…"
+    git apply "/tmp/$TARGET_BRANCH.patch"
 
-    git switch --quiet -c "$TARGET_BRANCH" "$ORIGIN_BRANCH"
-    git cherry-pick --quiet "$CP_DATA" > /dev/null 2>&1
-elif [ "$CP_TYPE" == "commit" ]; then
-    echo "Checking out commit code…"
+    # Commit the changes
+    git commit --quiet --allow-empty -am "patch"
+}
 
-    git switch --quiet -c "$TARGET_BRANCH" "$ORIGIN_BRANCH"
-    git cherry-pick --quiet "$CP_DATA" > /dev/null 2>&1
-fi
+fetch_commit() {
+    # Fetch the commit from the temporary repository
+    echo "Fetching commit…"
+    git fetch --quiet "$TEMP_DIR" "$COMMIT"
+}
 
-# Get the commit message of the changes
-if [ "$CP_TYPE" == "pr" ]; then
-    CHANGES_MESSAGE=$(curl -s "https://api.github.com/repos/$ORIGIN_REPO/pulls/$CP_DATA" | jq -r '.title' || git log -1 --pretty=format:"%s" "$TARGET_BRANCH")
-else
-    CHANGES_MESSAGE=$(git log -1 --pretty=format:"%s" "$TARGET_BRANCH")
-fi
+apply_patch_to_hydrogram() {
+    # Apply the patch to the Hydrogram repository
+    echo "Applying commit…"
+    applied_successfully=0
+    git cherry-pick -X ignore-all-space --no-commit "$COMMIT" || applied_successfully=1
 
-# Get the author of the changes
-CHANGES_AUTHOR=$(git log -1 --pretty=format:"%an <%ae>" "$TARGET_BRANCH")
+    if [ "$applied_successfully" == "0" ]; then
+        echo -e "\nPatch applied successfully!"
+        echo -e "Now you can commit the changes and push them to the repository with:"
+    else
+        echo -e "\nPatch applied with conflicts! Please resolve them and add the changes with:"
+        echo -e "git add .\n"
+        echo -e "If you would like to abort the cherry-pick, run:"
+        echo -e "git reset --merge\n"
 
-# Get the date of the changes
-CHANGES_DATE=$(git log -1 --pretty=format:"%aD" "$TARGET_BRANCH")
+        echo -e "and then commit the changes with:"
+    fi
 
-# Format the code
-echo "Formatting code…"
-ruff format --quiet .
+    echo "git commit --all \\"
+    echo "    --author='$CHANGES_AUTHOR' \\"
+    echo "    --date='$CHANGES_DATE' \\"
+    echo "    --message='$CHANGES_MESSAGE'"
+}
 
-# Commit the changes
-git commit --quiet --allow-empty -am "Format code"
+cleanup() {
+    # Clean up
+    echo -e "\nCleaning up…"
+    rm -rf "$TEMP_DIR"
+}
 
-# Create a patch between the two branches
-echo "Creating patch and saving to /tmp/$TARGET_BRANCH.patch…"
-git diff "$FORMATTED_ORIGIN_BRANCH" "$TARGET_BRANCH" | sed 's|pyrogram|hydrogram|g;s|Pyrogram|Hydrogram|g' > "/tmp/$TARGET_BRANCH.patch"
+main() {
+    if { [ "$1" != "pr" ] && [ "$1" != "branch" ] && [ "$1" != "commit" ]; } || [ -z "$2" ]; then
+        echo "Usage: $0 pr <PR number>"
+        echo "       $0 branch <branch name>"
+        echo "       $0 commit <commit hash>"
+        exit 1
+    fi
 
-# Apply the patch to the Hydrogram repository
-echo "Applying patch…"
-cd "$CWD"
-applied_successfully=0
-git apply "/tmp/$TARGET_BRANCH.patch" || applied_successfully=1
+    CP_TYPE=$1
+    CP_DATA=$2
+    CWD=$(pwd)
 
-if [ "$applied_successfully" == "0" ]; then
-    echo -e "\nPatch applied successfully!"
-    echo -e "Now you can commit the changes and push them to the repository with:\n"
-else
-    echo -e "\nPatch applied with conflicts! Please resolve them and add the changes with:"
-    echo -e "git add .\n"
+    TARGET_BRANCH="$CP_TYPE-$CP_DATA"
+    FORMATTED_ORIGIN_BRANCH="formatted-$ORIGIN_BRANCH"
 
-    echo -e "and then commit the changes with:"
-fi
+    # Clone the Pyrogram repository into a temporary directory
+    echo "Cloning Pyrogram repository…"
+    TEMP_DIR=$(mktemp -d)
+    git clone --quiet https://github.com/$ORIGIN_REPO.git "$TEMP_DIR"
 
-echo "git commit --all \\"
-echo "    --author='$CHANGES_AUTHOR' \\"
-echo "    --date='$CHANGES_DATE' \\"
-echo "    --message='$CHANGES_MESSAGE'"
+    # Switch to the $FORMATTED_ORIGIN_BRANCH branch
+    cd "$TEMP_DIR"
+    git switch --quiet -c "$FORMATTED_ORIGIN_BRANCH"
 
-# Clean up
-echo -e "\nCleaning up…"
-rm -rf "$TEMP_DIR"
+    # Hydrogramify the code (without the changes applied)
+    hydrogramify
+
+    # Fetch commits/branches to make them available for cherry-picking
+    if [ "$CP_TYPE" == "branch" ] || [ "$CP_TYPE" == "commit" ]; then
+        git fetch --quiet "$CWD" "$CP_DATA" || git fetch --quiet https://github.com/$ORIGIN_REPO.git "$CP_DATA" || true
+    fi
+
+    # Checkout the code
+    checkout_code
+
+    # Get the commit message of the changes
+    if [ "$CP_TYPE" == "pr" ]; then
+        CHANGES_MESSAGE=$(curl -s "https://api.github.com/repos/$ORIGIN_REPO/pulls/$CP_DATA" | jq -r '.title' || git log -1 --pretty=format:"%s")
+    else
+        CHANGES_MESSAGE=$(git log -1 --pretty=format:"%s")
+    fi
+
+    # Get the author of the changes
+    CHANGES_AUTHOR=$(git log -1 --pretty=format:"%an <%ae>")
+
+    # Get the date of the changes
+    CHANGES_DATE=$(git log -1 --pretty=format:"%aD")
+
+    # Get the commit hash of the changes
+    COMMIT=$(git log -1 --pretty=format:"%H")
+
+    # Hydrogramify the code (with the changes applied)
+    hydrogramify
+
+    # Create a patch between the two branches
+    create_patch
+
+    # Apply the patch to the formatted branch
+    apply_patch_to_formatted
+
+    cd "$CWD"
+
+    # Fetch the commit from the temporary repository
+    fetch_commit
+
+    # Apply the patch to the Hydrogram repository
+    apply_patch_to_hydrogram
+
+    # Clean up
+    cleanup
+}
+
+main "$@"
