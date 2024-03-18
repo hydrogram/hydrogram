94fbffc6cd43ab901defeffa64f37bfdbc9ddbc8
Mario A. <10923513+Midblyte@users.noreply.github.com>
2024-03-18 16:58:55 +0100

    fix: make explanation optional

--- hydrogram/methods/messages/send_poll.py
+++ hydrogram/methods/messages/send_poll.py
@@ -143,7 +143,7 @@ class SendPoll:
             await utils.parse_text_entities(
                 self, explanation, explanation_parse_mode, explanation_entities
             )
-        ).values()
+        ).values() if explanation else (None, None)
 
         reply_to = utils.get_reply_head_fm(message_thread_id, reply_to_message_id)
 
@@ -169,7 +169,7 @@ class SendPoll:
                     if correct_option_id is not None
                     else None,
                     solution=solution,
-                    solution_entities=solution_entities or [],
+                    solution_entities=None if solution is None else (solution_entities or []),
                 ),
                 message="",
                 silent=disable_notification,
