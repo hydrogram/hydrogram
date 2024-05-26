c946b03a98b0d51abd2ef7a152e62eabaa9a8905
Hitalo M. <hitalo331@outlook.com>
2024-05-26 18:06:06 -0300

    fix(client): idle and compose `ImportError`

--- hydrogram/__init__.py
+++ hydrogram/__init__.py
@@ -41,17 +41,21 @@ crypto_executor = ThreadPoolExecutor(1, thread_name_prefix="CryptoWorker")
 # ruff: noqa: E402
 from . import enums, errors, filters, handlers, raw, types
 from .client import Client
+from .methods.utilities.compose import compose
+from .methods.utilities.idle import idle
 
 __all__ = [
     "Client",
     "ContinuePropagation",
     "StopPropagation",
     "StopTransmission",
+    "compose",
     "crypto_executor",
     "enums",
     "errors",
     "filters",
     "handlers",
+    "idle",
     "raw",
     "types",
 ]
