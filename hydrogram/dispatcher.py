ee07b790ef5aecb9bb0d30e368734a6be2856ad5
Hitalo M. <hitalo331@outlook.com>
2024-07-20 22:11:16 -0300

    refactor(dispatcher): type hints

--- hydrogram/dispatcher.py
+++ hydrogram/dispatcher.py
@@ -17,13 +17,16 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Hydrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import asyncio
 import inspect
 import logging
 from collections import OrderedDict
+from typing import TYPE_CHECKING, Callable
 
 import hydrogram
-from hydrogram import utils
+from hydrogram import raw, types, utils
 from hydrogram.handlers import (
     CallbackQueryHandler,
     ChatJoinRequestHandler,
@@ -57,6 +60,12 @@ from hydrogram.raw.types import (
     UpdateUserStatus,
 )
 
+if TYPE_CHECKING:
+    from collections.abc import Awaitable
+
+    from hydrogram.handlers.handler import Handler
+
+
 log = logging.getLogger(__name__)
 
 
@@ -72,34 +81,52 @@ class Dispatcher:
     CHOSEN_INLINE_RESULT_UPDATES = (UpdateBotInlineSend,)
     CHAT_JOIN_REQUEST_UPDATES = (UpdateBotChatInviteRequester,)
 
-    def __init__(self, client: "hydrogram.Client"):
+    def __init__(self, client: hydrogram.Client):
         self.client = client
         self.loop = asyncio.get_event_loop()
-        self.handler_worker_tasks = []
-        self.locks_list = []
+        self.handler_worker_tasks: list[asyncio.Task] = []
+        self.locks_list: list[asyncio.Lock] = []
         self.updates_queue = asyncio.Queue()
-        self.groups = OrderedDict()
+        self.groups: dict[int, list[Handler]] = OrderedDict()
+        self.error_handlers: list[ErrorHandler] = []
         self._init_update_parsers()
-        self.error_handlers = []
 
     def _init_update_parsers(self):
-        self.update_parsers = {
-            Dispatcher.NEW_MESSAGE_UPDATES: self._message_parser,
-            Dispatcher.EDIT_MESSAGE_UPDATES: self._edited_message_parser,
-            Dispatcher.DELETE_MESSAGES_UPDATES: self._deleted_messages_parser,
-            Dispatcher.CALLBACK_QUERY_UPDATES: self._callback_query_parser,
-            Dispatcher.USER_STATUS_UPDATES: self._user_status_parser,
-            Dispatcher.BOT_INLINE_QUERY_UPDATES: self._inline_query_parser,
-            Dispatcher.POLL_UPDATES: self._poll_parser,
-            Dispatcher.CHOSEN_INLINE_RESULT_UPDATES: self._chosen_inline_result_parser,
-            Dispatcher.CHAT_MEMBER_UPDATES: self._chat_member_updated_parser,
-            Dispatcher.CHAT_JOIN_REQUEST_UPDATES: self._chat_join_request_parser,
-        }
-        self.update_parsers = {
-            key: value for key_tuple, value in self.update_parsers.items() for key in key_tuple
+        update_parsers = {
+            (
+                UpdateNewMessage,
+                UpdateNewChannelMessage,
+                UpdateNewScheduledMessage,
+            ): self._message_parser,
+            (UpdateEditMessage, UpdateEditChannelMessage): self._edited_message_parser,
+            (UpdateDeleteMessages, UpdateDeleteChannelMessages): self._deleted_messages_parser,
+            (UpdateBotCallbackQuery, UpdateInlineBotCallbackQuery): self._callback_query_parser,
+            (UpdateUserStatus,): self._user_status_parser,
+            (UpdateBotInlineQuery,): self._inline_query_parser,
+            (UpdateMessagePoll,): self._poll_parser,
+            (UpdateBotInlineSend,): self._chosen_inline_result_parser,
+            (UpdateChatParticipant, UpdateChannelParticipant): self._chat_member_updated_parser,
+            (UpdateBotChatInviteRequester,): self._chat_join_request_parser,
         }
 
-    async def _message_parser(self, update, users, chats):
+        self.update_parsers: dict[
+            type[raw.core.TLObject],
+            Callable[
+                ...,
+                Awaitable[tuple[types.Update, type[Handler]]] | tuple[types.Update, type[Handler]],
+            ],
+        ] = {}
+
+        for key_tuple, parser in update_parsers.items():
+            for key in key_tuple:
+                self.update_parsers[key] = parser
+
+    async def _message_parser(
+        self,
+        update: UpdateNewMessage,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.Message | None, type[MessageHandler]]:
         return (
             await hydrogram.types.Message._parse(
                 client=self.client,
@@ -111,38 +138,83 @@ class Dispatcher:
             MessageHandler,
         )
 
-    async def _edited_message_parser(self, update, users, chats):
+    async def _edited_message_parser(
+        self,
+        update: UpdateNewMessage,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.Message | None, type[EditedMessageHandler]]:
         parsed, _ = await self._message_parser(update, users, chats)
         return parsed, EditedMessageHandler
 
-    def _deleted_messages_parser(self, update, users, chats):
+    def _deleted_messages_parser(
+        self,
+        update: UpdateNewMessage,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[list[types.Message], type[DeletedMessagesHandler]]:
         return utils.parse_deleted_messages(self.client, update), DeletedMessagesHandler
 
-    async def _callback_query_parser(self, update, users, chats):
+    async def _callback_query_parser(
+        self,
+        update: UpdateBotCallbackQuery,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.CallbackQuery, type[CallbackQueryHandler]]:
         return await hydrogram.types.CallbackQuery._parse(
             self.client, update, users
         ), CallbackQueryHandler
 
-    def _user_status_parser(self, update, users, chats):
+    def _user_status_parser(
+        self,
+        update: UpdateUserStatus,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.User, type[UserStatusHandler]]:
         return hydrogram.types.User._parse_user_status(self.client, update), UserStatusHandler
 
-    def _inline_query_parser(self, update, users, chats):
+    def _inline_query_parser(
+        self,
+        update: UpdateBotInlineQuery,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.InlineQuery, type[InlineQueryHandler]]:
         return hydrogram.types.InlineQuery._parse(self.client, update, users), InlineQueryHandler
 
-    def _poll_parser(self, update, users, chats):
+    def _poll_parser(
+        self,
+        update: UpdateMessagePoll,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.Poll | None, type[PollHandler]]:
         return hydrogram.types.Poll._parse_update(self.client, update), PollHandler
 
-    def _chosen_inline_result_parser(self, update, users, chats):
+    def _chosen_inline_result_parser(
+        self,
+        update: UpdateBotInlineSend,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.ChosenInlineResult, type[ChosenInlineResultHandler]]:
         return hydrogram.types.ChosenInlineResult._parse(
             self.client, update, users
         ), ChosenInlineResultHandler
 
-    def _chat_member_updated_parser(self, update, users, chats):
+    def _chat_member_updated_parser(
+        self,
+        update: UpdateChatParticipant | UpdateChannelParticipant,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.ChatMemberUpdated, type[ChatMemberUpdatedHandler]]:
         return hydrogram.types.ChatMemberUpdated._parse(
             self.client, update, users, chats
         ), ChatMemberUpdatedHandler
 
-    def _chat_join_request_parser(self, update, users, chats):
+    def _chat_join_request_parser(
+        self,
+        update: UpdateBotChatInviteRequester,
+        users: dict[int, raw.types.User],
+        chats: dict[int, raw.types.Chat],
+    ) -> tuple[types.ChatJoinRequest, type[ChatJoinRequestHandler]]:
         return hydrogram.types.ChatJoinRequest._parse(
             self.client, update, users, chats
         ), ChatJoinRequestHandler
@@ -166,7 +238,7 @@ class Dispatcher:
 
             log.info("Stopped %s HandlerTasks", self.client.workers)
 
-    def add_handler(self, handler, group: int):
+    def add_handler(self, handler: Handler, group: int):
         async def fn():
             async with asyncio.Lock():
                 if isinstance(handler, ErrorHandler):
@@ -180,7 +252,7 @@ class Dispatcher:
 
         self.loop.create_task(fn())
 
-    def remove_handler(self, handler, group: int):
+    def remove_handler(self, handler: Handler, group: int):
         async def fn():
             async with asyncio.Lock():
                 if isinstance(handler, ErrorHandler):
@@ -197,32 +269,50 @@ class Dispatcher:
 
         self.loop.create_task(fn())
 
-    async def handler_worker(self, lock):
+    async def handler_worker(self, lock: asyncio.Lock):
         while True:
             packet = await self.updates_queue.get()
             if packet is None:
                 break
             await self._process_packet(packet, lock)
 
-    async def _process_packet(self, packet, lock):
+    async def _process_packet(
+        self,
+        packet: tuple[raw.core.TLObject, dict[int, types.Update], dict[int, types.Update]],
+        lock: asyncio.Lock,
+    ):
         try:
             update, users, chats = packet
             parser = self.update_parsers.get(type(update))
 
             if parser is not None:
-                if inspect.iscoroutinefunction(parser):
-                    parsed_update, handler_type = await parser(update, users, chats)
+                parsed_result = parser(update, users, chats)
+                if inspect.isawaitable(parsed_result):
+                    parsed_update, handler_type = await parsed_result
                 else:
-                    parsed_update, handler_type = parser(update, users, chats)
+                    parsed_update, handler_type = parsed_result
             else:
                 parsed_update, handler_type = (None, type(None))
 
             async with lock:
                 for group in self.groups.values():
                     for handler in group:
-                        await self._handle_update(
-                            handler, handler_type, parsed_update, update, users, chats
-                        )
+                        try:
+                            if parsed_update is not None:
+                                if isinstance(handler, handler_type) and await handler.check(
+                                    self.client, parsed_update
+                                ):
+                                    await self._execute_callback(handler, parsed_update)
+                                    break
+                            elif isinstance(handler, RawUpdateHandler):
+                                await self._execute_callback(handler, update, users, chats)
+                                break
+                        except (hydrogram.StopPropagation, hydrogram.ContinuePropagation) as e:
+                            if isinstance(e, hydrogram.StopPropagation):
+                                raise
+                        except Exception as exception:
+                            if parsed_update is not None:
+                                await self._handle_exception(parsed_update, exception)
         except hydrogram.StopPropagation:
             pass
         except Exception as e:
@@ -230,20 +320,7 @@ class Dispatcher:
         finally:
             self.updates_queue.task_done()
 
-    async def _handle_update(self, handler, handler_type, parsed_update, update, users, chats):
-        try:
-            if isinstance(handler, handler_type):
-                if await handler.check(self.client, parsed_update):
-                    await self._execute_callback(handler, parsed_update)
-            elif isinstance(handler, RawUpdateHandler):
-                await self._execute_callback(handler, update, users, chats)
-        except (hydrogram.StopPropagation, hydrogram.ContinuePropagation) as e:
-            if isinstance(e, hydrogram.StopPropagation):
-                raise
-        except Exception as exception:
-            await self._handle_exception(parsed_update, exception)
-
-    async def _handle_exception(self, parsed_update, exception):
+    async def _handle_exception(self, parsed_update: types.Update, exception: Exception):
         handled_error = False
         for error_handler in self.error_handlers:
             try:
@@ -260,7 +337,7 @@ class Dispatcher:
         if not handled_error:
             log.exception("Unhandled exception: %s", exception)
 
-    async def _execute_callback(self, handler, *args):
+    async def _execute_callback(self, handler: Handler, *args):
         if inspect.iscoroutinefunction(handler.callback):
             await handler.callback(self.client, *args)
         else:
