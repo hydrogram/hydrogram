395a2e71bda4e8b75990bbc5d79ac4e858f4dd6f
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-09-30 09:19:51 +0530

    check

--- hydrogram/types/input_message_content/input_poll_option.py
+++ hydrogram/types/input_message_content/input_poll_option.py
@@ -16,6 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from hydrogram.types.object import Object
